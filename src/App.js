ece1047cad8233419d037e9cb68f6c7a8b115bac
Tolu Oshinowo <toluooshy@gmail.com>
2024-05-12 15:40:08 -0400

    Styling changes.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@ function App() {
   return (
     <div
       style={{
+        position: "fixed",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
         color: "#ffffff",
         margin: "auto",
         justifyContent: "center",
         textAlign: "center",
         height: "100%",
-        backgroundColor: "#101828",
+        background:
+          "linear-gradient(0deg, rgba(16,42,86,1) 0%, rgba(62,71,132,1) 100%)",
         flex: 1,
       }}
     >
-      <img src={logo} style={{ maxWidth: 400 }} alt="logo" />
-      <p style={{ fontSize: 24, padding: 24, fontStyle: "italic" }}>
-        Coming soon!
-      </p>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <img src={logo} style={{ maxWidth: 400, marginLeft: 20 }} alt="logo" />
+        <p style={{ fontSize: 24, padding: 24, fontStyle: "italic" }}>
+          Coming soon!
+        </p>
+      </div>
     </div>
   );
 }
